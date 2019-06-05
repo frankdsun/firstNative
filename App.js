0b38f245b846c70787dd52dc04078b82a2a6d027
Frank Sun <frank.sun@franksun.ca>
2019-06-04 23:19:28 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <Text>
+        Hello World!
+      </Text>
+    );
+  }
+}
+
+export default App;
